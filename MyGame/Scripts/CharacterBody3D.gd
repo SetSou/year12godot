6ff1a38c960195387aa78556136e96cba4e9f212
SetSou <100171414+SetSou@users.noreply.github.com>
2024-06-18 10:52:31 +1200

    not added collider but...
    started making wall running

--- MyGame/Scripts/CharacterBody3D.gd
+++ MyGame/Scripts/CharacterBody3D.gd
@@ -11,13 +11,11 @@ const SENSITIVITY = 0.003
 const BOB_FREQ = 2
 const BOB_AMP = 0.08
 var t_bob = 0.0
-
+var wall_normal
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
-
 var default_height = 1.5
 var crouch_height = 0.5
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
 
 @onready var head = $Head
@@ -35,7 +33,12 @@ func _unhandled_input(event):
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(70))
-		
+	
+func wall_run():
+	if Input.is_action_pressed("forwards"):
+		if is_on_wall():
+			velocity.y = 0
+	
 func _physics_process(delta):
 	speed = WALK_SPEED
 	# Add the gravity.
@@ -43,7 +46,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and (is_on_floor() or is_on_wall()):
 		velocity.y = JUMP_VELOCITY
 	# Handle crouch.
 	if Input.is_action_pressed("crouch") and state != "sprinting":
@@ -55,6 +58,8 @@ func _physics_process(delta):
 	# Handle sprint.
 	if Input.is_action_pressed("sprint") and not Input.is_action_pressed("crouch"):
 		enter_sprint_state()
+	if state == "crouching":
+		speed = CROUCH_SPEED
 	
 	
 	# Get the input direction and handle the movement/deceleration.
@@ -78,7 +83,7 @@ func _physics_process(delta):
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
+	wall_run()
 	move_and_slide()
 
 func enter_normal_state():
